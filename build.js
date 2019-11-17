3dd0b1b1d0ecd2c6023a690ab60309ff988da1c7
kazuma1989 <funifuni.1204@gmail.com>
2019-11-17 20:55:57 +0900

    refactor: chalk shorthands

--- build.js
+++ build.js
@@ -5,17 +5,21 @@ const chalk = require("chalk");
 
 const card = path.resolve(__dirname, "./dist/card");
 
+const text = chalk.white;
+const label = chalk.white.bold;
+const mute = chalk.gray;
+
 // prettier-ignore
 const content = `
-${chalk.white     ("           Kazuma Ebina")} / ${chalk.white("@kazuma1989")}
+${text ("           Kazuma Ebina")} / ${text("@kazuma1989")}
 
-${chalk.white.bold("    Work:")}  ${chalk.white("Frontend web developer at Quipper/Recruit MP")}
+${label("    Work:")}  ${text("Frontend web developer at Quipper/Recruit MP")}
 
-${chalk.white.bold("  GitHub:")}  ${chalk.gray("https://github.com/")}${chalk.green("kazuma1989")}
-${chalk.white.bold("LinkedIn:")}  ${chalk.gray("https://linkedin.com/in/")}${chalk.blue("kazuma1989")}
-${chalk.white.bold("   Email:")}  ${chalk.cyan("kazuma.ebina@quipper.com")}
+${label("  GitHub:")}  ${mute("https://github.com/")}${chalk.green("kazuma1989")}
+${label("LinkedIn:")}  ${mute("https://linkedin.com/in/")}${chalk.blue("kazuma1989")}
+${label("   Email:")}  ${chalk.cyan("kazuma.ebina@quipper.com")}
 
-${chalk.white.bold("    Card:")}  ${chalk.red("npx")} ${chalk.white("kazuma1989")}
+${label("    Card:")}  ${chalk.red("npx")} ${text("kazuma1989")}
 `.trim();
 
 fs.writeFileSync(
